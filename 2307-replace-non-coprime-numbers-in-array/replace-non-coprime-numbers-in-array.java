b8a3966f8ba0e841d37442dc72b2ce6bc5fe3b60
Ayush Pratap <ayushprince110@gmail.com>
2025-09-16 21:32:38 +0530

    Time: 18 ms (92.78%) | Memory: 64.2 MB (43.30%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.java
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.java
@@ -2,48 +2,42 @@ import java.util.*;
 
 class Solution {
     public List<Integer> replaceNonCoprimes(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        
+        int n = nums.length;
+        int[] stack = new int[n];
+        int top = -1;
+
         for (int num : nums) {
             long current = num;
-            
-            while (!result.isEmpty()) {
-                long last = result.get(result.size() - 1);
+
+            while (top >= 0) {
+                long last = stack[top];
                 long gcd = gcd(last, current);
-                
-                if (gcd > 1) {  
-                    current = lcm(last, current, gcd);
-                    result.remove(result.size() - 1);
+
+                if (gcd > 1) {
+                    current = (last / gcd) * current;
+                    top--;
                 } else {
                     break;
                 }
             }
-            result.add((int) current);
+
+            stack[++top] = (int) current;
         }
-        
+
+        List<Integer> result = new ArrayList<>(top + 1);
+        for (int i = 0; i <= top; i++) {
+            result.add(stack[i]);
+        }
+
         return result;
     }
 
     private long gcd(long a, long b) {
         while (b != 0) {
-            long temp = b;
-            b = a % b;
-            a = temp;
+            long t = a % b;
+            a = b;
+            b = t;
         }
         return a;
-    }
-
-    private long lcm(long a, long b, long gcd) {
-        return (a / gcd) * b;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        int[] nums1 = {6, 4, 3, 2, 7, 6, 2};
-        System.out.println(sol.replaceNonCoprimes(nums1));
-
-        int[] nums2 = {2, 2, 1, 1, 3, 3, 3};
-        System.out.println(sol.replaceNonCoprimes(nums2));
     }
 }
