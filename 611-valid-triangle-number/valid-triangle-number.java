4d039ecb94f50159c603c0f997fd405398559ad7
Ayush Pratap <ayushprince110@gmail.com>
2025-09-26 17:00:24 +0530

    Time: 22 ms (99.95%) | Memory: 44.6 MB (5.68%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.java
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -1,24 +1,31 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        int count = 0;
 
-        for (int k = n - 1; k >= 2; k--) {
-            int i = 0;
-            int j = k - 1;
+        // Parallelize the outer loop over k
+        int count = IntStream.range(2, n) // 2 to n-1
+                .parallel()               // make it run in parallel
+                .map(k -> {
+                    int localCount = 0;
+                    int i = 0;
+                    int j = k - 1;
 
-            while (i < j) {
-                if (nums[i] + nums[j] > nums[k]) {
-                    count += (j - i);
-                    j--;
-                } else {
-                    i++;
-                }
-            }
-        }
+                    while (i < j) {
+                        if (nums[i] + nums[j] > nums[k]) {
+                            localCount += (j - i);
+                            j--;
+                        } else {
+                            i++;
+                        }
+                    }
+
+                    return localCount; // return count for this k
+                })
+                .sum(); // sum counts from all k
 
         return count;
     }
